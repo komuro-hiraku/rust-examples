6a165ca82ab4e6556c6cf18342ab77e83192738a
komuro.hiraku <komuro.hiraku@classmethod.jp>
2021-08-22 14:11:17 +0000

    Add Generics Cacher Struct Modify Closure

--- closure_sample/src/main.rs
+++ closure_sample/src/main.rs
@@ -4,21 +4,21 @@ use std::time::Duration;
 
 fn generate_workout(intensity: u32, random_number: u32) {
 
-    let expensive_closure = | num | {
+    let mut expensive_result = Cacher::new(|num| {
         println!("calculating slowly...");
         thread::sleep(Duration::from_secs(2));
         num
-    };
+    });
 
     if intensity < 25 {
         println!(
             "Today, do {} pushups!", // 腕立て
-            expensive_closure(intensity)
+            expensive_result.value(intensity)
         );
 
         println!(
             "Next, do {} situps!",  // 腹筋
-            expensive_closure(intensity)
+            expensive_result.value(intensity)
         );
     } else {
         if random_number == 3 {
@@ -28,7 +28,7 @@ fn generate_workout(intensity: u32, random_number: u32) {
         } else {
             println!(
                 "Today, run for {} minutes!",
-                expensive_closure(intensity)
+                expensive_result.value(intensity)
             );
         }
     }
@@ -46,4 +46,34 @@ fn main() {
     let example_closure = |x| x;
     let _s = example_closure(String::from("hello"));
     // let n = example_closure(5); // 上で String で推論した Closure なのでu32はダメ 　
+}
+
+struct Cacher<T>
+    where T: Fn(u32) -> u32
+{ 
+    calculation: T,
+    value: Option<u32>,
+}
+
+
+impl<T> Cacher<T>
+    where T: Fn(u32) -> u32 
+{ 
+    fn new(calculation: T) -> Cacher<T> {
+        Cacher {
+            calculation,
+            value: None,
+        }
+    }
+
+    fn value(&mut self, arg: u32) -> u32 {
+        match self.value {
+            Some(v) => v,
+            None => {
+                let v = (self.calculation)(arg);
+                self.value = Some(v);
+                v
+            },
+        }
+    }
 }
