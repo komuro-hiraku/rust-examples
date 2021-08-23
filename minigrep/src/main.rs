0f365cc4bdc0e07fa0b432c954f007a5f2199030
komuro.hiraku <komuro.hiraku@classmethod.jp>
2021-08-23 14:11:14 +0000

    fix Config::new() function

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -10,7 +10,7 @@ use minigrep::Config;
 fn main() {
 
     let config = Config::new(env::args()).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
