b6e36cab4c0f86f25081ec8436686717e5acf6da
komuro.hiraku <komuro.hiraku@classmethod.jp>
2021-08-23 13:53:44 +0000

    minigrep project の Config の args[1].clone() 等の記述を削除

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -62,13 +62,22 @@ pub struct Config {
 
 impl Config {
     // fn parse_config(args: &[String]) -> Config をリファクタ
-    pub fn new(args: &[String]) -> Result<Config, &'static str> {
+    pub fn new(mut args: std::env::Args) -> Result<Config, &'static str> {
+        args.next();
+
         if args.len() < 3 {
             return Err("not enough arguments");
         }
 
-        let query = args[1].clone();
-        let filename = args[2].clone();
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+
+        let filename = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file name"),
+        }
 
         let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
 
