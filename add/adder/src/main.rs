bc64f42f94d0550b42804b3057359506fffa8782
komuro.hiraku <komuro.hiraku@classmethod.jp>
2021-08-26 12:34:40 +0000

    Add extra lib. add-two

--- add/adder/src/main.rs
+++ add/adder/src/main.rs
@@ -1,7 +1,9 @@
 extern crate add_one;
+extern crate add_two;
 
 fn main() {
     let num = 10;
 
     println!("Hello, world! {} plus one is {}", num, add_one::add_one(num));
+    println!("Good bye world! {} plus two is {}", num, add_two::add_two(num));
 }
