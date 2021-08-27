ba75c024f7d27adcc4231ea12481447fe158b95a
komuro.hiraku <komuro.hiraku@classmethod.jp>
2021-08-27 08:39:30 +0000

    参照外し型強制のサンプルコード追加

--- smartpointer_sample/src/main.rs
+++ smartpointer_sample/src/main.rs
@@ -5,6 +5,7 @@ enum List { // 再帰的な構造のためコンパイルできない
 }
 
 use List::{Cons, Nil};
+use std::ops::Deref;
 
 fn main() {
     let b = Box::new(5);    // わざとヒープに置く。あんまり意味はない
@@ -17,9 +18,35 @@ fn main() {
     println!("list: {:?}", list);
 
     let x = 5;
-    let y = Box::new(x);
+    let y = MyBox::new(x);
 
     assert_eq!(5, x);
-    assert_eq!(5, *y);
+    assert_eq!(5, *y); // *(y.deref()) と同等
+    assert_eq!(5, *(y.deref()));
+
+    let m = MyBox::new(String::from("Rust"));
+    hello(&m);
+    hello(&(*m)[..]); // 参照外し型強制がなかったらこうなる
+    // (*m) で MyBox<String> を String に参照外し
+    // &, [..] で String を &str （文字列スライス）に変換
 }
 
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &T {
+        &self.0
+    }
+}
+
+fn hello(name: &str) {
+    println!("Hello, {}!", name);
+}
