6f54f6428ef67383430a28ad8ce79c293edb99b3
komuro.hiraku <komuro.hiraku@classmethod.jp>
2021-08-27 12:16:31 +0000

    RefCell<T> のサンプル実装

--- refcell_sample/src/main.rs
+++ refcell_sample/src/main.rs
@@ -6,8 +6,33 @@
 //          RefCell<T> では不変借用も可変借用も実行時に精査できる
 // RefCell<T> : 実行時に精査される可変借用を許可するので、RefCell<T> が不変でも、RefCell<T> 内の値を可変化できる
 
-// 不変な値の中の値を可変化することは、内部可変性パターン。
+// 不変な値の中の値を可変化することは、内部可変性パターン
+
+#[derive(Debug)]
+enum List {
+
+    Cons(Rc<RefCell<i32>>, Rc<List>),
+    Nil,
+}
+
+use List::{Cons, Nil};
+use std::rc::Rc;
+use std::cell::RefCell;
+
 fn main() {
-    let x = 5;
+    // let x = 5;
     // let y = &mut x; // 不変なローカル変数 `x` を可変で借用することはできない
+
+    let value = Rc::new(RefCell::new(5));
+
+    let a = Rc::new(Cons(Rc::clone(&value), Rc::new(Nil)));
+
+    let b = Cons(Rc::new(RefCell::new(6)), Rc::clone(&a));
+    let c = Cons(Rc::new(RefCell::new(10)), Rc::clone(&a));
+
+    *value.borrow_mut() += 10;
+
+    println!("a after = {:?}", a);
+    println!("b after = {:?}", b);
+    println!("c after = {:?}", c);
 }
